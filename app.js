ee9ba29c3a931340bb366f137eb9abcc2781417c
KateStearns <katestearns@mac.com>
2015-07-22 19:06:06 -0700

    Add 3rd page

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ function createAboutMeContent(){
 	'Claymation has freaked me out since I was a little kid.',
 	'I am a Washington State University grad (Go Cougs!)',
 	'I have been living in the Boston area for the past 3 years',
-	'tbd',
-	'Painting is a way for me to relax, I love painting landscapes with watercolor or anything via my computer'
+	' ',
+	'I have a 7 year-old nephew who I think is amazing.',
+	'I am a Game of Thrones nerd, I read the books too.',
+	'Watercolor painting is a hobby of mine, I have since learned how to pain on a mac.'
 	];
 
 	//Use a loop to append a bullet for each text string
@@ -26,22 +28,32 @@ function createAboutMeContent(){
 }
 createAboutMeContent();
 
+// Write code that feeds images from JavaScript
 function generateCodeProjectImages(){
-var codeProjectCollection = ['codeProjectCalc.jpg', 'codeProjectGOT.jpg', 'codeProjectWater.jpg'];
+	//Set Header
+	var h2element = document.getElementById('codeProjectHeader');
+	var codeProjectHeader = 'Code Projects...';
+	h2element.textContent = codeProjectHeader;
 
-$('#codeProject0').attr('src', codeProjectCollection[0]);
-$('#codeProject1').attr('src', codeProjectCollection[1]);
-$('#codeProject2').attr('src', codeProjectCollection[2]);
+	// Declare an array with the 3 fileNames
+	var codeProjectCollection = ['codeProjectCalc.jpg', 'codeProjectGOT.jpg', 'codeProjectWater.jpg'];
+
+	$('#codeProject0').attr('src', codeProjectCollection[0]);
+	$('#codeProject1').attr('src', codeProjectCollection[1]);
+	$('#codeProject2').attr('src', codeProjectCollection[2]);
 
 }
 generateCodeProjectImages();
 
+//Create a function to generate the Gallery thumbnails and header
 function generateGalleryThumbNail(){
+	//create an empty array to push the file names into
 	var imageCollection = [];
 	for(var j=0; j < 16; j++){
 		var imageFileName = 'Image' + j + '.jpg';
 		imageCollection.push(imageFileName);
 
+		//Use jQuery to feed file names to the document.
 		$('#galleryImg0').attr('src', imageCollection[0]);
 		$('#galleryImg1').attr('src', imageCollection[1]);
 		$('#galleryImg2').attr('src', imageCollection[2]);
@@ -50,21 +62,36 @@ function generateGalleryThumbNail(){
 		$('#galleryImg5').attr('src', imageCollection[15]);
 	};
 }
-	// $('#thumbNail0').attr('src', imageFileName[i]);
 generateGalleryThumbNail();
 
+// Use jQuery to disolve between the home page content and the gallery content.
 function navigationBar() {
 	$('#galleryPage').hide();
 }
 navigationBar();
 
 $('#galleryButton').on('click', function(){
-	$('#galleryPage').fadeIn(2000);
+	$('#galleryPage').fadeIn(1000);
 	$('#homePage').fadeOut();
 })
 
 $('#homeButton').on('click', function(){
-	$('#homePage').fadeIn(2000);
+	$('#homePage').fadeIn(1000);
 	$('#galleryPage').fadeOut();
 })
+
+// Use jQuery to load code goals page
+// function codeProject() {
+// 	$('#codeProjectsButton').on('click', funtion (){
+// 		$('#testLoad').load('codeProjects.html');
+// 	})
+// }
+// codeProject()
+function testFunction(){
+	window.location.href = 'codeProjects.html';
+}
+
+function goHome(){
+	window.location.href = 'index.html';
+}
 
